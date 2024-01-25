b1a07cd47b7524ace0eadfb1b3115ab33e10b870
eswar <eswar250493@gmail.com>
2024-01-25 11:40:53 +0530

    change in route

--- src/App.jsx
+++ src/App.jsx
@@ -16,20 +16,21 @@ function App() {
     <>
      <nav className='flex text-lg bg-blue-400 hover:bg-blue-500 text-white p-4 font-semibold items-center justify-between w-full'>
       <img src={Logo} className='h-12 w-12 object-contain' />
-      <Link to='/'>Home</Link>
-      <Link to='/product'>Products</Link>
-     <Link to='/cart'> <IoMdCart size={35} /></Link>
+      <Link to='/basic_ecommerce/'>Home</Link>
+      <Link to='/basic_ecommerce/product'>Products</Link>
+     <Link to='/basic_ecommerce/cart'> <IoMdCart size={35} /></Link>
 
       
      </nav>
      
-    </>
+    
     <Routes>
-    <Route path='/' element={<Display />} />
-    <Route path='/product' element={<Home/>} />
-    <Route path='/product/:id' element={<Description/>} />
-    <Route path='/cart' element={<Cart/>} />
+    <Route path='/basic_ecommerce' element={<Display />} />
+    <Route path='/basic_ecommerce/product' element={<Home/>} />
+    <Route path='/basic_ecommerce/product/:id' element={<Description/>} />
+    <Route path='/basic_ecommerce/cart' element={<Cart/>} />
     </Routes>
+    </>
     </BrowserRouter>
   )
 }
--- src/Product.jsx
+++ src/Product.jsx
@@ -4,7 +4,7 @@ const Product = ({item}) => {
     const {id,title,price,image}= item;
   return (
    <>
-  <Link to={`/product/${id}`}><div className="shadow-md min-h-[350px] flex justify-center items-center flex-col p-1">
+  <Link to={`/basic_ecommerce/product/${id}`}><div className="shadow-md min-h-[350px] flex justify-center items-center flex-col p-1">
    <img className="h-[150px] w-[200px] object-contain" src={image} alt="" />
    <div className="flex flex-col  ml-2 mt-3 w-full justify-center">
     <p className="text-sm  w-[170px]">{title}</p>
